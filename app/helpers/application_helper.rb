1bba47ec9e3b94de24a08d3e305062341475bd40
Kuba Nowak <kubanowak9755@gmail.com>
2025-11-04 00:07:48 +0100

    feat: create user preferences views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,21 +7,21 @@ module ApplicationHelper
   # Returns array of [label, value] pairs for budget select dropdown
   def budget_options
     Enums::Budget.string_values.map do |value|
-      [value.humanize.titleize, value]
+      [ value.humanize.titleize, value ]
     end
   end
 
   # Returns array of [label, value] pairs for accommodation select dropdown
   def accommodation_options
     Enums::Accommodation.string_values.map do |value|
-      [value.humanize.titleize, value]
+      [ value.humanize.titleize, value ]
     end
   end
 
   # Returns array of [label, value] pairs for activity checkboxes
   def activity_options
     Enums::Activity.string_values.map do |value|
-      [value.humanize.titleize, value]
+      [ value.humanize.titleize, value ]
     end
   end
 
@@ -30,16 +30,16 @@ module ApplicationHelper
     Enums::EatingHabit.string_values.map do |value|
       # Custom labels for eating habits
       label = case value
-              when 'restaurants_only'
+      when 'restaurants_only'
                 'Restaurants Only'
-              when 'self_prepared'
+      when 'self_prepared'
                 'Self-Prepared'
-              when 'mix'
+      when 'mix'
                 'Mix'
-              else
+      else
                 value.humanize.titleize
-              end
-      [label, value]
+      end
+      [ label, value ]
     end
   end
 end
